10715216d3a1ce81b77a7c3ccfdf1c07f3d52ff6
Le Phuoc Thang Nguyen <thang@fromttm.com>
2016-10-10 17:15:54 -0700

    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,16 @@
-    var myVideo = document.getElementById("video1");
-    myVideo.play();
+var myVideo = document.getElementById("video1");
+myVideo.play();
+function playersize(){
+    if ($(window).width() > 1400) {
+        $(".headerInfo").css("height", $(window).height()*.9);
+    }
+    else {
+        $(".headerInfo").css("height", $(window).width()*0.5625);
+    }
+}
+playersize();
+//Resize To Fit Frame
+$(window).on('load, resize',function(){
+    playersize();
+});
+setTimeout(function(){playersize();},500);
