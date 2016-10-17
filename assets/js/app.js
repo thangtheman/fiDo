bc3e1cc66c385863cf059937ad21e6d2f258c981
Le Phuoc Thang Nguyen <thang@fromttm.com>
2016-10-17 16:29:23 -0700

    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ var myVideo = document.getElementById("video1");
 myVideo.play();
 function playersize(){
     if ($(window).width() > 1400) {
-        $(".headerInfo").css("height", $(window).height()*.9);
+        $(".headerInfo").css("height", $(window).height()*.8);
     }
     else {
         $(".headerInfo").css("height", $(window).width()*0.5625);
@@ -14,3 +14,16 @@ $(window).on('load, resize',function(){
     playersize();
 });
 setTimeout(function(){playersize();},500);
+
+var cb=1;
+function changebackground(){
+	cb=cb+1;
+	if(cb>4){
+		cb=1;
+	}
+	setTimeout(function(){
+		$("#meetTheHost").css('background-image','url(assets/img/bg/dog-'+cb+'.jpg)');
+		changebackground();
+	},10000);	
+}
+changebackground();
